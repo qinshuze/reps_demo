d20935f908472ad45a166339a0b42df6426da2dc
1191249254@qq.com <1191249254@qq.com>
2020-09-07 21:20:06 +0800

    first commit

---
+++ helloworld/Hello/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hello;
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 12/22/16
+ * Time: 10:40 AM
+ */
+class HelloWorld
+{
+    public function say_hello(){
+        echo "hello world";
+    }
+
+}
